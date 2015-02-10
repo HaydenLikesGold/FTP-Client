fb9d4b5d500eb151af649cf4848681a126583727
Hayden Goldstien <haydengoldstien@gmail.com>
2015-02-10 17:26:54 -0600

    Refactor: Change file name to stop attributeError

--- ftpclient/Client.py
+++ ftpclient/Client.py
@@ -1,126 +1,123 @@
 from socket import *
+
+import ftp_response_handler
 import response
 
-#Error Helpers
-def raise_errors_for_values(response, list_of_values):
-	response_value = int(response[0:1])
-	for return_code in list_of_values:
-		if response_value == return_code:
-			raise RuntimeError("FTP error: " + response)
-
-def raise_failures_for_values(response, list_of_values):
-	response_value = int(response[0:1])
-	for return_code in list_of_values:
-		if response_value == return_code:
-			raise RuntimeError("FTP failure: " + response)
-
-def raise_alert_if_value_isnt(response, list_of_values):
-	response_value = int(response[0:1])
-	for return_code in list_of_values:
-		if response_value == return_code:
-			return
-	raise RuntimeError("Unexpected FTP response: " + response)
-
-#Login Process
+# Login Process
 def user_login_process():
-	user_response_code = user_entry()
-	if (user_response_code == 3):
-		pass_response_code = password_entry()
-		if (pass_response_code == 3):
-			acct_entry()
+    user_response_code = user_entry()
+    if (user_response_code == 3):
+        pass_response_code = password_entry()
+        if (pass_response_code == 3):
+            acct_entry()
+
 
 def user_entry():
-	_socketFile.write("USER anonymous\r\n")
-	_socketFile.flush()
-	response_x = response.get_response(_socketFile)
-	validate_user_and_pass_response(response_x)
-	return int(response_x[0:1])
+    _socketFile.write("USER anonymous\r\n")
+    _socketFile.flush()
+    response_x = response.get_response(_socketFile)
+    validate_user_and_pass_response(response_x)
+    return int(response_x[0:1])
+
 
 def password_entry():
-	_socketFile.write('PASS anonymous@gustavus.edu\r\n')
-	_socketFile.flush()
-	pass_response = response.get_response(_socketFile)
-	validate_user_and_pass_response(pass_response)
-	return int(pass_response[0:1])
+    _socketFile.write('PASS anonymous@gustavus.edu\r\n')
+    _socketFile.flush()
+    pass_response = response.get_response(_socketFile)
+    validate_user_and_pass_response(pass_response)
+    return int(pass_response[0:1])
+
 
 def validate_user_and_pass_response(response):
-	raise_errors_for_values(response, [1])
-	raise_failures_for_values(response, [4,5])
-	raise_alert_if_value_isnt(response, [2,3])
+    ftp_response_handler.raise_ftp_error_for_values(response, [1])
+    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
+    ftp_response_handler.raise_error_if_not_in_given_values(response, [2, 3])
+
 
 def acct_entry():
-	_socketFile.write('ACCT anonymous\r\n')
-	_socketFile.flush()
-	acct_response = response.get_response(_socketFile)
-	validate_acct_response(acct_response)
+    _socketFile.write('ACCT anonymous\r\n')
+    _socketFile.flush()
+    acct_response = response.get_response(_socketFile)
+    validate_acct_response(acct_response)
+
 
 def validate_acct_response(response):
-	raise_errors_for_values(response, [1,3])
-	raise_failures_for_values(response, [4,5])
-	raise_alert_if_value_isnt(response, [2])
+    ftp_response_handler.raise_ftp_error_for_values(response, [1, 3])
+    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
+    ftp_response_handler.raise_error_if_not_in_given_values(response, [2])
 
-#Connection Process
+
+# Connection Process
 def connect_to_princeton_server():
-	serverName = 'ftp.cs.princeton.edu'
-	serverPort = 21
-	_clientSocket.connect((serverName, serverPort))
-	_clientSocket.close()
-	response.get_response(_socketFile)
+    serverName = 'ftp.cs.princeton.edu'
+    serverPort = 21
+    _clientSocket.connect((serverName, serverPort))
+    _clientSocket.close()
+    response.get_response(_socketFile)
+
 
 def connect_to_new_socket():
-	pasv_port = send_pasv_command()
-	_createdSocket.connect(pasv_port)
-	_createdSocket.close()
+    pasv_port = send_pasv_command()
+    _createdSocket.connect(pasv_port)
+    _createdSocket.close()
+
 
 def send_pasv_command():
-	_socketFile.write('PASV\r\n')
-	_socketFile.flush()
-	newPortInformation = response.get_response(_socketFile)
-	validate_pasv_and_quit_command(newPortInformation)
-	return response.response_to_port_tuple(newPortInformation)
+    _socketFile.write('PASV\r\n')
+    _socketFile.flush()
+    newPortInformation = response.get_response(_socketFile)
+    validate_pasv_and_quit_command(newPortInformation)
+    return response.response_to_port_tuple(newPortInformation)
+
 
 def validate_pasv_and_quit_command(response):
-	raise_errors_for_values(response, [1,3])
-	raise_failures_for_values(response, [4,5])
-	raise_alert_if_value_isnt(response, [2])
+    ftp_response_handler.raise_ftp_error_for_values(response, [1, 3])
+    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
+    ftp_response_handler.raise_error_if_not_in_given_values(response, [2])
+
 
 def send_retr_command():
-	_socketFile.write('RETR /pub/cs126/nbody/3body.txt\r\n')
-	_socketFile.flush()
-	validate_retr_command(response.get_response(_socketFile))
-	validate_retr_command(response.get_response(_socketFile))
+    _socketFile.write('RETR /pub/cs126/nbody/3body.txt\r\n')
+    _socketFile.flush()
+    validate_retr_command(response.get_response(_socketFile))
+    validate_retr_command(response.get_response(_socketFile))
+
 
 def validate_retr_command(response):
-	raise_errors_for_values(response, [3])
-	raise_failures_for_values(response, [4,5])
-	raise_alert_if_value_isnt(response, [1,2])
+    ftp_response_handler.raise_ftp_error_for_values(response, [3])
+    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
+    ftp_response_handler.raise_error_if_not_in_given_values(response, [1, 2])
+
 
 def retrieve_data():
-	send_retr_command()
-	return response.get_retr_response(_createdFile)
+    send_retr_command()
+    return response.get_retr_response(_createdFile)
+
 
 def close_server_connection():
-	_socketFile.write('QUIT\r\n')
-	_socketFile.flush()
-	validate_pasv_and_quit_command(response.get_response(_socketFile))
+    _socketFile.write('QUIT\r\n')
+    _socketFile.flush()
+    validate_pasv_and_quit_command(response.get_response(_socketFile))
+
 
 def get_server_response_less_basic():
-	global _clientSocket
-	global _createdSocket
-	global _socketFile
-	global _createdFile
+    global _clientSocket
+    global _createdSocket
+    global _socketFile
+    global _createdFile
 
-	_clientSocket = socket(SOCK_DGRAM)
-	_createdSocket = socket(SOCK_DGRAM)
-	_socketFile = _clientSocket.makefile(mode = "rw")
-	_createdFile = _createdSocket.makefile(mode = "rw")
+    _clientSocket = socket(SOCK_DGRAM)
+    _createdSocket = socket(SOCK_DGRAM)
+    _socketFile = _clientSocket.makefile(mode="rw")
+    _createdFile = _createdSocket.makefile(mode="rw")
 
-	connect_to_princeton_server()
-	user_login_process()
-	connect_to_new_socket()
-	data = retrieve_data()
+    connect_to_princeton_server()
+    user_login_process()
+    connect_to_new_socket()
+    data = retrieve_data()
 
-	close_server_connection()
-	return data
+    close_server_connection()
+    return data
+
 
 print(get_server_response_less_basic())
---
+++ ftpclient/ftp_response_handler.py
@@ -0,0 +1,25 @@
+"""Manages errors from a given response and a list of values"""
+
+def raise_ftp_error_for_values(response, list_of_values):
+    """ftp errors passed in based on RFC"""
+    response_value = int(response[0:1])
+    for return_code in list_of_values:
+        if response_value == return_code:
+            raise RuntimeError("FTP error: " + response)
+
+
+def raise_ftp_failure_for_values(response, list_of_values):
+    """ftp failures passed in based on RFC"""
+    response_value = int(response[0:1])
+    for return_code in list_of_values:
+        if response_value == return_code:
+            raise RuntimeError("FTP failure: " + response)
+
+
+def raise_error_if_not_in_given_values(response, list_of_values):
+    """Expected values passed in based on RFC"""
+    response_value = int(response[0:1])
+    for return_code in list_of_values:
+        if response_value == return_code:
+            return
+    raise RuntimeError("Unexpected FTP response: " + response)
