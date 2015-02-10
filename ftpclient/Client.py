c828c2ad70d75797535fc00b9c68ea60dfa3e39a
Hayden Goldstien <haydengoldstien@gmail.com>
2015-02-10 17:38:44 -0600

    Refactor: Breakout Lifecycle process

--- ftpclient/Client.py
+++ ftpclient/Client.py
@@ -1,60 +1,7 @@
 from socket import *
 
-import ftp_response_handler
-import response
 import login
-
-# Connection Process
-def connect_to_princeton_server():
-    serverName = 'ftp.cs.princeton.edu'
-    serverPort = 21
-    _clientSocket.connect((serverName, serverPort))
-    _clientSocket.close()
-    response.get_response(_socketFile)
-
-
-def connect_to_new_socket():
-    pasv_port = send_pasv_command()
-    _createdSocket.connect(pasv_port)
-    _createdSocket.close()
-
-
-def send_pasv_command():
-    _socketFile.write('PASV\r\n')
-    _socketFile.flush()
-    newPortInformation = response.get_response(_socketFile)
-    validate_pasv_and_quit_command(newPortInformation)
-    return response.response_to_port_tuple(newPortInformation)
-
-
-def validate_pasv_and_quit_command(response):
-    ftp_response_handler.raise_ftp_error_for_values(response, [1, 3])
-    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
-    ftp_response_handler.raise_error_if_not_in_given_values(response, [2])
-
-
-def send_retr_command():
-    _socketFile.write('RETR /pub/cs126/nbody/3body.txt\r\n')
-    _socketFile.flush()
-    validate_retr_command(response.get_response(_socketFile))
-    validate_retr_command(response.get_response(_socketFile))
-
-
-def validate_retr_command(response):
-    ftp_response_handler.raise_ftp_error_for_values(response, [3])
-    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
-    ftp_response_handler.raise_error_if_not_in_given_values(response, [1, 2])
-
-
-def retrieve_data():
-    send_retr_command()
-    return response.get_retr_response(_createdFile)
-
-
-def close_server_connection():
-    _socketFile.write('QUIT\r\n')
-    _socketFile.flush()
-    validate_pasv_and_quit_command(response.get_response(_socketFile))
+import lifecycle
 
 
 def get_server_response_less_basic():
@@ -68,12 +15,12 @@ def get_server_response_less_basic():
     _socketFile = _clientSocket.makefile(mode="rw")
     _createdFile = _createdSocket.makefile(mode="rw")
 
-    connect_to_princeton_server()
+    lifecycle.connect_to_princeton_server(_socketFile, _clientSocket)
     login.user_login_process(_socketFile)
-    connect_to_new_socket()
-    data = retrieve_data()
+    lifecycle.connect_to_new_socket(_createdSocket, _socketFile)
+    data = lifecycle.retrieve_data(_socketFile, _createdFile)
 
-    close_server_connection()
+    lifecycle.close_server_connection(_socketFile)
     return data
 
 
