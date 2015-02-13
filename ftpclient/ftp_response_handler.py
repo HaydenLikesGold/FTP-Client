cabe2662417754e0fdde63bdba2a47258e808046
Hayden Goldstien <haydengoldstien@gmail.com>
2015-02-13 13:58:06 -0600

    Refactor: Finish movie processes to response handler

--- ftpclient/ftp_response_handler.py
+++ ftpclient/ftp_response_handler.py
@@ -1,5 +1,6 @@
 """Manages errors from a given response and a list of values"""
 
+
 def raise_ftp_error_for_values(response, list_of_values):
     """ftp errors passed in based on RFC"""
     response_value = int(response[0:1])
@@ -31,7 +32,19 @@ def validate_pasv_and_quit_command(response):
     raise_error_if_not_in_given_values(response, [2])
 
 
+def validate_user_and_pass_response(response):
+    raise_ftp_error_for_values(response, [1])
+    raise_ftp_failure_for_values(response, [4, 5])
+    raise_error_if_not_in_given_values(response, [2, 3])
+
+
 def validate_retr_command(response):
     raise_ftp_error_for_values(response, [3])
     raise_ftp_failure_for_values(response, [4, 5])
     raise_error_if_not_in_given_values(response, [1, 2])
+
+
+def validate_acct_response(response):
+    raise_ftp_error_for_values(response, [1, 3])
+    raise_ftp_failure_for_values(response, [4, 5])
+    raise_error_if_not_in_given_values(response, [2])
--- ftpclient/login.py
+++ ftpclient/login.py
@@ -1,43 +1,38 @@
 import ftp_response_handler
 import response
 
+
+USER_COMMAND = "USER anonymous\r\n"
+PASSWORD_COMMAND = 'PASS anonymous@gustavus.edu\r\n'
+ACCOUNT_COMMAND = 'ACCT anonymous\r\n'
+
+
 def user_login_process(socket_file):
     user_response_code = user_entry(socket_file)
-    if (user_response_code == 3):
+    if user_response_code == 3:
         pass_response_code = password_entry(socket_file)
-        if (pass_response_code == 3):
+        if pass_response_code == 3:
             acct_entry()
 
+
 def user_entry(socket_file):
-    socket_file.write("USER anonymous\r\n")
+    socket_file.write(USER_COMMAND)
     socket_file.flush()
     response_x = response.get_response(socket_file)
-    validate_user_and_pass_response(response_x)
+    ftp_response_handler.validate_user_and_pass_response(response_x)
     return int(response_x[0:1])
 
 
 def password_entry(socket_file):
-    socket_file.write('PASS anonymous@gustavus.edu\r\n')
+    socket_file.write(PASSWORD_COMMAND)
     socket_file.flush()
     pass_response = response.get_response(socket_file)
-    validate_user_and_pass_response(pass_response)
+    ftp_response_handler.validate_user_and_pass_response(pass_response)
     return int(pass_response[0:1])
 
 
-def validate_user_and_pass_response(response):
-    ftp_response_handler.raise_ftp_error_for_values(response, [1])
-    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
-    ftp_response_handler.raise_error_if_not_in_given_values(response, [2, 3])
-
-
 def acct_entry(socket_file):
-    socket_file.write('ACCT anonymous\r\n')
+    socket_file.write(ACCOUNT_COMMAND)
     socket_file.flush()
     acct_response = response.get_response(socket_file)
-    validate_acct_response(acct_response)
-
-
-def validate_acct_response(response):
-    ftp_response_handler.raise_ftp_error_for_values(response, [1, 3])
-    ftp_response_handler.raise_ftp_failure_for_values(response, [4, 5])
-    ftp_response_handler.raise_error_if_not_in_given_values(response, [2])
+    ftp_response_handler.validate_acct_response(acct_response)
