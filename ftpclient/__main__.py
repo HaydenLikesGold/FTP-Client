a521025f74ca8acb82ea391d908a3214d9002cb6
Hayden Goldstien <haydengoldstien@gmail.com>
2015-02-13 15:14:44 -0600

    Refactor: Rely on command-line args

--- ftpclient/__main__.py
+++ ftpclient/__main__.py
@@ -2,14 +2,9 @@ import sys
 from file_transfer import FileTransfer
 
 def run_transfer():
-    if len(sys.argv) == 1:
-        server_name = 'ftp.cs.princeton.edu'
-        server_port = 21
-        file_path = '/pub/cs126/nbody/3body.txt'
-    else:
-        server_name = str(sys.argv[1])
-        server_port = int(sys.argv[2])
-        file_path = str(sys.argv[3])
+    server_name = str(sys.argv[1])
+    server_port = int(sys.argv[2])
+    file_path = str(sys.argv[3])
 
     x = FileTransfer(server_name, server_port, file_path)
     x.ftp_process()
